5088d3fb9d0ed210f515aa068c8caeccafea9089
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-05-12 11:39:49 +0900

    Create switch case 점수에 따라 학점.c

---
+++ 조건문/switch case 점수에 따라 학점.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int score;
+  
+  printf("학점을 입력하세요 (1~100)\n");
+  scanf("%d", &score);
+  switch (score/10){
+    case 10 : printf("A"); break; 
+    case 9 : printf("A"); break;
+    case 8 : printf("B"); break;
+    case 7 : printf("C"); break;
+    case 6 : printf("D"); break; 
+    case 5 : printf("E"); break;
+    default: printf("F"); break;
+  }
+}
