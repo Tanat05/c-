00bb81eb19c4517b345508fca97cd5a3dc3b832d
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-05-17 09:52:34 +0900

    Update switch case 점수에 따라 학점.c

--- 조건문/switch case 점수에 따라 학점.c
+++ 조건문/switch case 점수에 따라 학점.c
@@ -14,3 +14,30 @@ int main(){
     default: printf("F"); break;
   }
 }
+
+
+
+#include <stdio.h>
+
+int main(){
+  int score;
+  
+  printf("학점을 입력하세요 (1~100)\n");
+  scanf("%d", &score);
+  
+  if (score > 100 || score < 1){
+    printf("범위 내의 수를 입력해주세요");
+  }
+  else {  
+    switch (score/10){
+      case 10 : printf("학점은 A입니다."); break; 
+      case 9 : printf("학점은 A입니다."); break;
+      case 8 : printf("학점은 B입니다."); break;
+      case 7 : printf("학점은 C입니다."); break;
+      case 6 : printf("학점은 D입니다."); break; 
+      default: printf("학점은 F입니다."); break;
+      }
+  }
+  
+  return 0;
+}
