c14f905f54bc3ffa0b6683b92f9a9cb03be06f02
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-08 13:59:33 +0900

    Create 소수 판별.c

---
+++ 반복문/소수 판별.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int i, answer;
+  scanf("%d", &answer);
+
+  for (i=2;i<answer;i++){
+    if (answer%i==0)
+      break;
+  }
+  if (answer == i) printf("소수입니다.");
+  else printf("소수가 아닙니다.");
+  return 0;
+}  
