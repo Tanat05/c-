418a610ab6f3b81175be7601b075272760a59ae4
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-15 14:21:28 +0900

    Update 가장 큰 운동장.c

--- 반복문실력쌓기/가장 큰 운동장.c
+++ 반복문실력쌓기/가장 큰 운동장.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-  int a1,a2,max=0,i;
+  int a1,a2,max=0,n;
   printf("운동장 3개의 가로, 세로의 길이를 차례대로 입력하시요.\n");
-  for (i=1;i<=3;i++)
+  for (n=1;n<=3;n++)
     scanf("%d %d", &a1, &a2);
     if (a1*a2>max)
       max = a1*a2;
