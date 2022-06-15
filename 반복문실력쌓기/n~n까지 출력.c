ba0a934e8ad29edc1ed048f83baccc4a9b806187
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-15 14:01:31 +0900

    Update n~n까지 출력.c

--- 반복문실력쌓기/n~n까지 출력.c
+++ 반복문실력쌓기/n~n까지 출력.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
   int n,i;
-  scanf("두 수를 입력하세요 : %d %d", &n, &i);
+  printf("두 수를 입력하세요 : ");
+  scanf("%d %d", &n, &i);
   for (n;n<=i;n++)
     printf("%d ", n);
 }
