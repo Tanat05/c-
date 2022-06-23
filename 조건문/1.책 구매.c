e1c890ff499f1fa5426281d8f62be8f39aba3118
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-23 11:25:17 +0900

    Update 1.책 구매.c

--- 조건문/1.책 구매.c
+++ 조건문/1.책 구매.c
@@ -6,7 +6,7 @@ int main(){
   printf("당신이 가진 용돈은 얼마인가요? : ");
   scanf("%d", &money);
   if (book < money){
-    printf("착을 구입하였습니다. 이제 남은 용돈은 %d입니다.", money-book);
+    printf("책을 구입하였습니다. 이제 남은 용돈은 %d입니다.", money-book);
   }
 
   return 0;
