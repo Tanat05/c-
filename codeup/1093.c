374a8f82b15d6924b2286c508235bb78b5623138
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-08-25 15:37:42 +0900

    Create 1093.c

---
+++ codeup/1093.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+  int n, i, t;
+  int a[10000]={};
+  scanf("%d", &n);
+  for(i=1; i<=n; i++)
+  {
+    scanf("%d", &t);
+    a[t]=a[t]+1;
+  }
+  for(i=1; i<=23; i++)
+  {
+    printf("%d ", a[i]);
+  }
+}
