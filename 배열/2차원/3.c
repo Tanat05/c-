a53cff9e765aa95f4b6ab6949d10b9d4c026c26d
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-01 14:53:48 +0900

    Update 3.c

--- 배열/2차원/3.c
+++ 배열/2차원/3.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-int main(){
-  int floor, ho;
-  int apt[3][4] = {{111000,125000,136000,98000},{136000,142000,112000,135000},{127000,133000,155000,143000}};
-  int sumfloor[3]= {0,0,0};
-  for (floor=0;floor<3;floor++){
-    for (ho=0;ho<4;ho++){
-      sumfloor[floor] += apt[floor][ho];
-    }
-  }
-  for (floor=0;floor<3;floor++){
-    printf("%0.1f\n", (double)sumfloor[floor]/4);
-  }
-  return  0;
-}
+
