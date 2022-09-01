ef13d40a720609d63efd8cafe9b529f187323c82
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-01 15:26:44 +0900

    Update 2.c

--- 배열/실력쌓기/2.c
+++ 배열/실력쌓기/2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
   int i,n=0;
   char text[100]={0};
-  scanf("%[^\n]s", text);
-  for (i=0;i<100;i++){
+  gets("%s", text);
+  for (i=0;i<strlen(text);i++){
     printf("%c", text[99-i]);
   }
   return  0;
