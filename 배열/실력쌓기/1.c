bf48651ea88f824a285994ac960d7c4e1a148fae
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-01 15:40:28 +0900

    Update 1.c

--- 배열/실력쌓기/1.c
+++ 배열/실력쌓기/1.c
@@ -17,3 +17,21 @@ int main(){
   }
   return  0;
 }
+
+
+#include <stdio.h>
+int main(){
+  int i;
+  char text[21];
+  printf("암호를 입력하시오 : ");
+  scanf("%s", text);
+  printf("결과 1 : ");
+  for (i=0;i!='\n';i++){
+    printf("%c", text[i]+2);
+  }
+  printf("\n결과 2 : ");
+  for (i=0;i!='\n';i++){
+    printf("%c", (text[i]*7)%80+48);
+  }
+  return  0;
+}
