c8b25e03c5036161bae7bdb446fff4a2e43c2603
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-05 11:18:10 +0900

    Update 2.c

--- 배열/실력쌓기/2.c
+++ 배열/실력쌓기/2.c
@@ -9,3 +9,17 @@ int main(){
   }
   return  0;
 }
+
+
+
+
+#include <stdio.h>
+int main(){
+  int i,n=0;
+  char text[100]={0};
+  scanf("%[^\n]s", text);
+  for (i=0;i<100;i++){
+    printf("%c", text[99-i]);
+  }
+  return  0;
+}
