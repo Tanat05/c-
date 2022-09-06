d4a7999fa1c6732e93339e752577a2739df407fd
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-06 11:08:12 +0900

    Update 4.c

--- 배열/실력쌓기/4.c
+++ 배열/실력쌓기/4.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+int main(){
+  int i, n=0 , x=0;
+  char dna[10];
+  scanf("%s", dna);
+  for (i=0;i<10;i++){
+    n += dna[i]%(10^i);
+  }
+  
+  if (n%7 == 4){
+    printf("범인");
+  }
+  else{
+    printf("일반인");
+  }  
+}
+
+
+
+#include <stdio.h>
 #include <stdlib.h>
 int main(){
   int i, n=0 , x=0;
