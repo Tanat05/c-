fe03d830aeb4072948ddbf1a266efee297b6af79
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-15 14:49:54 +0900

    Create 1463.c

---
+++ codeup/1463.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  int x, y, n, num=1;
+  int a[100][100];
+  scanf("%d", &n);
+  for (x=0;x<n;x++){
+    for (y=0;y<n;y++){
+      a[x][y] = num++;
+    }
+  }
+  for (x=0;x<n;x++){
+    for (y=0;y<n;y++){
+      printf("%d ", a[y][n-1-x]);
+      }
+    printf("\n");
+  }
+}
