fb1b39d559c18376247e37a5107efb59621f7a3b
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-22 15:27:39 +0900

    Update 4.c

--- 함수/실력쌓기/4.c
+++ 함수/실력쌓기/4.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-int f(int n[3]){
-  int a, b;
-  a = n[0]*100+n[1]*10+n[2];
-  b = n[2]*100+n[1]*10+n[0];
-  return  a+b;
+int f(int n){
+  int reverse=0;
+
+  while(n>0){
+    reverse *= 10;
+    reverse += n%10;
+    n /= 10;
+  }
+  return reverse;
 } 
 int main(){
-  int n[3]={0};
-  scanf("%1d%1d%1d", &n[0], &n[1], &n[2]);
-  printf("%d", f(n));
+  int n;
+  scanf("%d", &n);
+  if (n+f(n)==f(n+f(n)))
+    printf("YES");
+  else
+    printf("NO");
 }  
