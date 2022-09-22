aabdf6a6cefa836f91d6e69467993599478b62f8
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-09-22 15:31:37 +0900

    Update 2.c

--- 함수/실력쌓기/2.c
+++ 함수/실력쌓기/2.c
@@ -6,15 +6,11 @@ int main(){
   int a, b, c, d, sum;
   printf("4개의 윷 상태를 입력하세요.\n(0:뒤집어지지 않은 상태, 1:뒤집어진 상태) : ");
   scanf("%d %d %d %d", &a, &b, &c, &d);
-  sum = f(a, b, c, d);
-  if(sum==0)
-    printf("모");
-  else if (sum==1)
-    printf("도");
-  else if (sum==2)
-    printf("개");
-  else if (sum==3)
-    printf("걸");
-  else
-    printf("윷");
-}  
+  switch(f(a, b, c, d)){
+    case 1: printf("도"); break;
+    case 2: printf("개"); break;
+    case 3: printf("걸"); break;
+    case 4: printf("윷"); break;
+    default: printf("모");
+  }  
+}
