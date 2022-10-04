58d67f8749280bb9c42d59973649408f0eaeb375
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-10-04 10:48:18 +0900

    Create 암호 해독.c

---
+++ 프로젝트 해결하기/암호 해독.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+char text1[101], text2[101];
+
+void input(){
+  gets(text1);
+}
+
+void process(char text){
+  int i;
+  for (i=0;i<strlen(text1);i++)
+  	if (text1[i] != 0)
+  		if (text1[i] == 'a' || text1[i] == 'b' || text1[i] == 'c')
+  			text2[i] = text1[i] + 23;
+  		else if (text1[i] == ' ')
+  			text2[i] = ' ';
+    	else
+			text2[i] = text1[i] - 3;
+}
+
+int output(){
+  printf("%s", text2);
+}
+
+int main(){
+  input();
+  process(text1);
+  output();
+  return 0;
+}
