a4b3aa34c3b1c6be31547a59fa63774c0d31c841
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-10-04 10:25:23 +0900

    Create 만능 디지털 카드키.c

---
+++ 프로젝트 해결하기/만능 디지털 카드키.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int n1, n2, n3, id=0, i;
+
+int small(int a, int b){
+  if (a>b)
+    return a;
+  else
+    return b;
+}
+
+void input(){
+  scanf("%d %d %d", &n1, &n2, &n3);
+}
+
+int process(int a, int b, int c){
+  int i;
+  for (i=1;i<small(a,small(b,c));i++){
+    if (a%i==0 && b%i==0 && c%i==0)
+      id = i;
+  }
+  return id;
+}
+
+int output(){
+  printf("%d", id);
+}
+
+int main(){
+  input();
+  process(n1, n2, n3);
+  output();
+  return 0;
+}
