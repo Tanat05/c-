bbe5b0607e61a70e673cff0902c84ef73413d3bb
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-10-04 10:03:28 +0900

    Update 철근 자르기.c

--- 프로젝트 해결하기/철근 자르기.c
+++ 프로젝트 해결하기/철근 자르기.c
@@ -20,17 +20,7 @@ void output(){
   }
   else{
     for(i=0;i<cnt;i++){
-      printf("F - ");
-      if (i+1<10){
-        printf("000");
-      }
-      else if (i+1<100){
-        printf("00");
-      }
-      else if (i+1<1000){
-        printf("0");
-      }
-      printf("%d\n", i+1);
+      printf("F - %04d\n", i+1);
     }
   }
 }
